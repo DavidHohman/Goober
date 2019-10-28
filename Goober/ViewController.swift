e0d2fa12f27447cd72c4929ab330dd7e5bf35c00
David Hohman <davidhohman@Davids-MacBook-Air.local>
2019-10-28 14:41:31 -0400

    Initial Commit

---
+++ Goober/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Goober
+//
+//  Created by David Hohman on 10/28/19.
+//  Copyright © 2019 David Hohman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var gooberImage: UIImageView!
+    
+    @IBOutlet weak var gooberTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+
+    @IBAction func gooberButton(_ sender: UIButton) {
+    }
+    
+}
+
